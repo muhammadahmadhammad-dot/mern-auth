22016993e5fbc8547dfd4f28af6bbe80cf371127
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-03-27 17:36:34 +0500

    profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
 import { login, register } from "../controller/userController.js";
+import authMiddleware from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
 router.post("/login-check",login) 
 router.post("/register",register)
-
+router.get("/profile",authMiddleware, (req, res)=>{
+    return res.json(req.user) // req.user  set by  middleware
+})
 export default router;
