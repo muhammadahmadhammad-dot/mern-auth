e8e1d606e36a5e603969be53b00edd4b379c91cc
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-03-27 17:51:53 +0500

    logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,7 @@ router.post("/register",register)
 router.get("/profile",authMiddleware, (req, res)=>{
     return res.json(req.user) // req.user  set by  middleware
 })
+router.post("/logout",authMiddleware,(req,res)=>{
+    return res.clearCookie("token",{ httpOnly: true}).json({msg:"Logout successfully"})
+})
 export default router;
